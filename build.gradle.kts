dc475dfc0d71ba0aefb375d82e26f2edc2de2983
heosangbeom <luv_3_u@naver.com>
2024-11-23 15:51:32 +0900

    :sparkles:feat(array): log4j 추가 및 멀티스레드 상황 테스트
    - log4j 추가
      - 로그 확인용
    
    - 멀티스레스 상황 테스트
      - 동시성 문제 발생 테스트

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
+
 }
 
 tasks.test {
---
+++ src/main/kotlin/thread/util/LoggingUtil.kt
@@ -0,0 +1,6 @@
+package thread.util
+
+import org.slf4j.LoggerFactory
+
+
+inline fun <reified T> T.logger() = LoggerFactory.getLogger(T::class.java)!!
